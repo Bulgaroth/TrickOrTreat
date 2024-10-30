39f90288c6a1db02153c02ffbd7b96cf71048b12
Bulgaroth <72599747+Bulgaroth@users.noreply.github.com>
2024-10-30 18:38:48 +0100

    Music implementation

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField, Range(0, 1)] private float[] maxVolumes;
+    [SerializeField] private float fadeRate = 1;
+
+    private readonly List<AudioSource> tracks = new();
+	private int trackLvl;
+
+    private void Awake()
+    {
+        foreach(Transform aS in transform) tracks.Add(aS.GetComponent<AudioSource>());
+    }
+
+	public void ChangeTrackLevel(bool add)
+	{
+		if (add) StartCoroutine(FadeIn(++trackLvl));
+        else while(trackLvl > 0) StartCoroutine(FadeOut(--trackLvl));
+	}
+
+	private IEnumerator FadeOut(int trackIndex)
+	{
+		while (tracks[trackIndex].volume >= 0)
+		{
+			tracks[trackIndex].volume -= Time.deltaTime * fadeRate;
+            yield return null;
+		}
+	}
+
+    private IEnumerator FadeIn(int trackIndex)
+    {
+        while (tracks[trackIndex].volume <= maxVolumes[trackIndex])
+        {
+            tracks[trackIndex].volume += Time.deltaTime * fadeRate;
+            yield return null;
+        }
+    }
+}

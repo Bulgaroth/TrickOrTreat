cc72c6905e06658ccee77bb792d8ecce787699cf
Bulgaroth <72599747+Bulgaroth@users.noreply.github.com>
2024-10-31 10:32:38 +0100

    Footsteps sounds

--- Assets/Scripts/HeadBobbing.cs
+++ Assets/Scripts/HeadBobbing.cs
@@ -2,104 +2,134 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem.Controls;
+using Random = UnityEngine.Random;
 
 public class HeadBobbing : MonoBehaviour
 {
-    #region Attributes
+	#region Attributes
 
-    public bool _isEnable;
+	private const float FOOTSTEP_COOLDOWN = 0.2f;
 
-    [SerializeField] private SC_HeadBobbingData _data;
+	public bool _isEnable;
+	public bool alreadyStepped;
 
-    private Camera _camera;
-    [SerializeField] private Transform _cameraHolder;
+	[SerializeField] private SC_HeadBobbingData _data;
+	[SerializeField] private AudioSource footStepsAudio;
+	[Serializable] private class FootstepArray
+	{
+		public string tag;
+		public List<AudioClip> footsteps;
+	}
+	[SerializeField] private List<FootstepArray> footstepsSounds;
 
-    private float _toggleSpeed = 3.0f;
-    private Vector3 _startPos;
-    private CharacterController _controller;
+	private Camera _camera;
+	[SerializeField] private Transform _cameraHolder;
 
-    private bool isMoving;
-    
-    #endregion
+	private float _toggleSpeed = 3.0f;
+	private Vector3 _startPos;
+	private CharacterController _controller;
 
-    #region Unity API
+	private bool isMoving;
+	public enum GroundType { Concrete, Grass, Tile, Car }
+	public GroundType groundType;
+	
+	#endregion
 
-    void Awake()
-    {
-        _controller = GetComponent<CharacterController>();
-        _camera = Camera.main;
-        _startPos = _camera.transform.localPosition;
-    }
+	#region Unity API
 
-    void Update()
-    {
-        if (!_isEnable) return;
-        
-        CheckMotion();
-        
-        if (!isMoving)
-            ResetPosition();
-        //_camera.LookAt(FocusTarget());
-    }
+	void Awake()
+	{
+		_controller = GetComponent<CharacterController>();
+		_camera = Camera.main;
+		_startPos = _camera.transform.localPosition;
+	}
 
-    #endregion
+	void Update()
+	{
+		if (!_isEnable) return;
+		
+		CheckMotion();
+		
+		if (!isMoving)
+			ResetPosition();
+		//_camera.LookAt(FocusTarget());
+	}
 
-    #region Methods
+	#endregion
 
-    private void CheckMotion()
-    {
-        var velocity = _controller.velocity;
-        float speed = new Vector3(velocity.x, 0, velocity.z).magnitude;
+	#region Methods
 
-        isMoving = speed > _toggleSpeed;
-        
-        if (!isMoving) return;
-        //if (!_controller.isGrounded) return;
-        
-        PlayMotion(FootStepMotion());
-    }
-    
-    private Vector3 FootStepMotion()
-    {
-        float frequency = _data.frequency;
-        float amplitude = _data.amplitude;
-        
-        Vector3 pos = Vector3.zero;
-        pos.y += Mathf.Sin(Time.time * frequency) * amplitude;
-        pos.x += Mathf.Cos(Time.time * frequency / 2) * amplitude * 2;
-        //Debug.Log($"StepMotion: {pos}");
-        return pos;
-    }
+	private void CheckMotion()
+	{
+		var velocity = _controller.velocity;
+		float speed = new Vector3(velocity.x, 0, velocity.z).magnitude;
 
-    private void PlayMotion(Vector3 motion)
-    {
-        //Debug.Log("Play Motion");
-        
-        _camera.transform.localPosition += motion;
-    }
+		isMoving = speed > _toggleSpeed;
+		
+		if (!isMoving) return;
+		//if (!_controller.isGrounded) return;
+		
+		PlayMotion(FootStepMotion());
+	}
+	
+	private Vector3 FootStepMotion()
+	{
+		float frequency = _data.frequency;
+		float amplitude = _data.amplitude;
+		
+		Vector3 pos = Vector3.zero;
+		pos.y += Mathf.Sin(Time.time * frequency) * amplitude;
+		if (pos.y <= 0.002 && pos.y >= -0.002) PlayFootStep();
+		pos.x += Mathf.Cos(Time.time * frequency / 2) * amplitude * 2;
+		//Debug.Log($"StepMotion: {pos}");
+		return pos;
+	}
 
-    private void ResetPosition()
-    {
-        if (Vector3.Distance(_camera.transform.localPosition, _startPos) < 0.001f)
-        {
-            _camera.transform.localPosition = _startPos;    
-            return;
-        }
-        
-        _camera.transform.localPosition = Vector3.Lerp(_camera.transform.localPosition, _startPos, 1.5f * Time.deltaTime);
-    }
+	private void PlayMotion(Vector3 motion)
+	{
+		//Debug.Log("Play Motion");
+		
+		_camera.transform.localPosition += motion;
+	}
 
-    private Vector3 FocusTarget()
-    {
-        var position = transform.position;
-        Vector3 pos = new Vector3(position.x,
-            position.y + _cameraHolder.localPosition.y,
-            position.z);
-        pos += _cameraHolder.forward * 15.0f;
-        return pos;
-    }
+	private void ResetPosition()
+	{
+		if (Vector3.Distance(_camera.transform.localPosition, _startPos) < 0.001f)
+		{
+			_camera.transform.localPosition = _startPos;
+			return;
+		}
+		
+		_camera.transform.localPosition = Vector3.Lerp(_camera.transform.localPosition, _startPos, 1.5f * Time.deltaTime);
+	}
 
-    #endregion
-    
+	private Vector3 FocusTarget()
+	{
+		var position = transform.position;
+		Vector3 pos = new Vector3(position.x,
+			position.y + _cameraHolder.localPosition.y,
+			position.z);
+		pos += _cameraHolder.forward * 15.0f;
+		return pos;
+	}
+
+	private void PlayFootStep()
+	{
+		if (alreadyStepped) return;
+		alreadyStepped = true;
+		StartCoroutine(FootStepCooldown());
+
+		print(groundType);
+		var footstepArray = footstepsSounds[(int)groundType];
+		footStepsAudio.PlayOneShot(footstepArray.footsteps[Random.Range(0, footstepArray.footsteps.Count)]);
+	}
+
+	#endregion
+
+	private IEnumerator FootStepCooldown()
+	{
+		yield return new WaitForSeconds(FOOTSTEP_COOLDOWN);
+		alreadyStepped = false;
+	}
+	
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,37 +6,39 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    #region Attributes
+	#region Attributes
 
-    [Header("=== Player ===")]
-    [SerializeField] private SC_PlayerData playerData;
+	[Header("=== Player ===")]
+	[SerializeField] private SC_PlayerData playerData;
+	[SerializeField] private LayerMask groundLayers;
+	[SerializeField] private HeadBobbing headBobbing;
 
-    [SerializeField] private int playerHP;
-    
-    [SerializeField] private Vector2 cameraRange;
-    [SerializeField] private float gravity = 100.0f;
-    
-    private Camera mainCamera;
-    private CharacterController _controller;
-    
-    private float verticaRotation;
-    private Vector3 movements;
-    private Vector2 mouseRotation;
-    
-    // Gun Related
-    [Header("=== Gun ===")] 
-    [SerializeField] private List<Rigidbody> bulletList;
-    [SerializeField] private float bulletPower;
-    [SerializeField] private Transform bulletSpawnPoint;
+	[SerializeField] private int playerHP;
+	
+	[SerializeField] private Vector2 cameraRange;
+	[SerializeField] private float gravity = 100.0f;
+	
+	private Camera mainCamera;
+	private CharacterController _controller;
+	
+	private float verticaRotation;
+	private Vector3 movements;
+	private Vector2 mouseRotation;
 
+	[SerializeField] private HealthBar healthBar;
+	
+	// Gun Related
+	[Header("=== Gun ===")] 
+	[SerializeField] private List<Rigidbody> bulletList;
+	[SerializeField] private float bulletPower;
+	[SerializeField] private Transform bulletSpawnPoint;
+	#endregion
 
-    #endregion
+	#region Events
 
-    #region Events
-
-    [HideInInspector] public UnityEvent<int> Heal;
-    [HideInInspector] public UnityEvent<int> TakeDamage;
-    [HideInInspector] public UnityEvent Die;
+	[HideInInspector] public UnityEvent<int> Heal;
+	[HideInInspector] public UnityEvent<int> TakeDamage;
+	[HideInInspector] public UnityEvent Die;
 
 	[HideInInspector] public UnityEvent<int> AddMaxLife;
 	[HideInInspector] public UnityEvent<float> AddSpeed;
@@ -49,82 +51,99 @@ public class PlayerController : MonoBehaviour
 	#region Unity API
 
 	private void Awake()
-    {
-        mainCamera = Camera.main;
-        _controller = GetComponent<CharacterController>();
-        playerHP = playerData.playerBaseHP;
-    }
+	{
+		mainCamera = Camera.main;
+		_controller = GetComponent<CharacterController>();
+		playerHP = playerData.playerBaseHP;
+	}
 
-    void Update()
-    {
-        MovePlayer();
-        RotateCamera();
-    }
+	void Update()
+	{
+		MovePlayer();
+		RotateCamera();
+		CheckGroundType();
+	}
 
-    #endregion
+	#endregion
 
-    #region Methods
+	#region Methods
 
-    void MovePlayer()
-    {
-        float curSpeedX = playerData.playerSpeed * movements.x;
-        float curSpeedZ = playerData.playerSpeed * movements.z;
-        Vector3 moveDirection = (transform.forward * curSpeedZ) + (transform.right * curSpeedX);
-        
-        ApplyGravity(ref moveDirection);
-        
-        _controller.Move( moveDirection * Time.deltaTime);
-    }
-    
-    void ApplyGravity(ref Vector3 moveDirection)
-    {
-        if (_controller.isGrounded) return;
+	void MovePlayer()
+	{
+		float curSpeedX = playerData.playerSpeed * movements.x;
+		float curSpeedZ = playerData.playerSpeed * movements.z;
+		Vector3 moveDirection = (transform.forward * curSpeedZ) + (transform.right * curSpeedX);
+		
+		ApplyGravity(ref moveDirection);
+		
+		_controller.Move( moveDirection * Time.deltaTime);
+	}
+	
+	void ApplyGravity(ref Vector3 moveDirection)
+	{
+		if (_controller.isGrounded) return;
 
-        moveDirection.y -= gravity * Time.deltaTime;
-    }
+		moveDirection.y -= gravity * Time.deltaTime;
+	}
 
-    void RotateCamera()
-    {
-        transform.Rotate(0, mouseRotation.x,0);
-        
-        verticaRotation -= mouseRotation.y;
-        verticaRotation = Mathf.Clamp(verticaRotation, cameraRange.x, cameraRange.y);
-        mainCamera.transform.localRotation = Quaternion.Euler(verticaRotation, 0, 0);
-    }
+	void RotateCamera()
+	{
+		transform.Rotate(0, mouseRotation.x,0);
+		
+		verticaRotation -= mouseRotation.y;
+		verticaRotation = Mathf.Clamp(verticaRotation, cameraRange.x, cameraRange.y);
+		mainCamera.transform.localRotation = Quaternion.Euler(verticaRotation, 0, 0);
+	}
 
 
-    void Shoot()
-    {
-        // Instantiate the projectile at the position and rotation of this transform
-        Rigidbody clone;
-        clone = Instantiate(bulletList[Random.Range(0, bulletList.Count)], bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+	void Shoot()
+	{
+		// Instantiate the projectile at the position and rotation of this transform
+		Rigidbody clone;
+		clone = Instantiate(bulletList[Random.Range(0, bulletList.Count)], bulletSpawnPoint.position, bulletSpawnPoint.rotation);
 
-        // Give the cloned object an initial velocity along the current
-        // object's Z axis
-        clone.velocity = mainCamera.transform.forward * bulletPower;
-    }
-    
-    
-    #endregion
+		// Give the cloned object an initial velocity along the current
+		// object's Z axis
+		clone.velocity = mainCamera.transform.forward * bulletPower;
+	}
 
-    #region Event Handlers
+	void CheckGroundType()
+	{
+		if(Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, 1, groundLayers))
+		{
+			print(hit.collider.tag);
+			HeadBobbing.GroundType groundType = hit.collider.tag switch
+			{
+				"Ground_Grass" => HeadBobbing.GroundType.Grass,
+				"Ground_Concrete" => HeadBobbing.GroundType.Concrete,
+				"Ground_Tile" => HeadBobbing.GroundType.Tile,
+				_ => HeadBobbing.GroundType.Car
+			};
 
-    private void OnEnable()
-    {
-        AddMaxLife.AddListener(OnAddMaxLife);
-        AddDmg.AddListener(OnAddDmg);
-        AddSpeed.AddListener(OnAddSpeed);
+			headBobbing.groundType = groundType;
+		}
+	}
+
+	#endregion
+
+	#region Event Handlers
+
+	private void OnEnable()
+	{
+		AddMaxLife.AddListener(OnAddMaxLife);
+		AddDmg.AddListener(OnAddDmg);
+		AddSpeed.AddListener(OnAddSpeed);
 		AddFireRate.AddListener(OnAddFireRate);
 		AddPowerUpEfficiency.AddListener(OnAddPowerUpEfficiency);
-        
-        Heal.AddListener(OnHeal);
-        TakeDamage.AddListener(OnTakeDamage);
-        Die.AddListener(OnDie);
-    }
+		
+		Heal.AddListener(OnHeal);
+		TakeDamage.AddListener(OnTakeDamage);
+		Die.AddListener(OnDie);
+	}
 
 
-    private void OnDisable()
-    {
+	private void OnDisable()
+	{
 		AddMaxLife.RemoveListener(OnAddMaxLife);
 		AddDmg.RemoveListener(OnAddDmg);
 		AddSpeed.RemoveListener(OnAddSpeed);
@@ -132,79 +151,84 @@ public class PlayerController : MonoBehaviour
 		AddPowerUpEfficiency.RemoveListener(OnAddPowerUpEfficiency);
 
 		Heal.RemoveListener(OnHeal);
-        TakeDamage.RemoveListener(OnTakeDamage);
-        Die.RemoveListener(OnDie);
-    }
+		TakeDamage.RemoveListener(OnTakeDamage);
+		Die.RemoveListener(OnDie);
+	}
 
-    private void OnHeal(int heal)
-    {
-        Debug.Log($"Player heal {heal} hp");
-        
-        playerHP += heal;
-        
-        if (playerHP > playerData.playerBaseHP)
-            playerHP = playerData.playerBaseHP;
-    }
+	private void OnHeal(int heal)
+	{
+		Debug.Log($"Player heal {heal} hp");
+		
+		playerHP += heal;
+		
+		if (playerHP > playerData.playerBaseHP)
+			playerHP = playerData.playerBaseHP;
 
-    void OnTakeDamage(int damage)
-    {
-        Debug.Log($"Player take {damage} damage");
-        
-        playerHP -= damage;
-        
-        if (playerHP <= 0)
-            Die.Invoke();
-    }
+		healthBar.SetHealth(playerHP);
+	}
 
-    void OnDie()
-    {
-        Debug.Log($"Player died");
-    }
+	void OnTakeDamage(int damage)
+	{
+		Debug.Log($"Player take {damage} damage");
+		
+		playerHP -= damage;
+		healthBar.SetHealth(playerHP);
 
-    void OnAddMaxLife(int amount)
-    {
-        playerData.playerBaseHP += amount;
-        // TODO Update UI.
+		if (playerHP <= 0)
+			Die.Invoke();
+	}
 
-    }
-    void OnAddDmg(int amount) => playerData.addedDamage += amount;
-    void OnAddSpeed(float multiplier) => playerData.playerSpeed *= multiplier;
-    void OnAddFireRate(float multiplier) => playerData.fireRate *= multiplier;
+	void OnDie()
+	{
+		Debug.Log($"Player died");
+	}
 
-    void OnAddPowerUpEfficiency(float multiplier)
-    {
-        playerData.healAmount *= (int)multiplier;
-        playerData.slowTime *= multiplier;
-    }
+	void OnAddMaxLife(int amount)
+	{
+		playerData.playerBaseHP += amount;
+		// TODO Update UI.
+
+		healthBar.SetMaxHealth(playerData.playerBaseHP);
+
+	}
+	void OnAddDmg(int amount) => playerData.addedDamage += amount;
+	void OnAddSpeed(float multiplier) => playerData.playerSpeed *= multiplier;
+	void OnAddFireRate(float multiplier) => playerData.fireRate *= multiplier;
+
+	void OnAddPowerUpEfficiency(float multiplier)
+	{
+		playerData.healAmount *= (int)multiplier;
+		playerData.slowTime *= multiplier;
+	}
 
 	#endregion
 
 	#region Inputs
 
 	public void Move(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            movements = new Vector3(context.ReadValue<Vector2>().x, 0f, context.ReadValue<Vector2>().y);
-        }
-        else if (context.canceled)
-        {
-            movements = Vector3.zero;
-        }
-    }
+	{
+		if (context.performed)
+		{
+			movements = new Vector3(context.ReadValue<Vector2>().x, 0f, context.ReadValue<Vector2>().y);
+		}
+		else if (context.canceled)
+		{
+			movements = Vector3.zero;
+		}
+	}
 
-    public void Look(InputAction.CallbackContext context)
-    {
-        mouseRotation = context.ReadValue<Vector2>();
-    }
+	public void Look(InputAction.CallbackContext context)
+	{
+		mouseRotation = context.ReadValue<Vector2>();
+	}
 
-    public void Fire(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            Shoot();
-        }
-    }
+	public void Fire(InputAction.CallbackContext context)
+	{
+		if (context.performed)
+		{
+			Shoot();
+		}
+	}
 
-    #endregion
+	#endregion
 }
